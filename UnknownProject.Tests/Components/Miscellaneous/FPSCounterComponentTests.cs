afbf6a1cae15392b139d12e5da43e00eb740f56c
Jannis Mattheis <jannisma@web.de>
2016-05-05 19:50:50 +0200

    add tests for fpscounter and component collection

---
+++ UnknownProject.Tests/Components/Miscellaneous/FPSCounterComponentTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using UnknownProject.Components.Miscellaneous;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnknownProject.Core;
+using FakeItEasy;
+using Microsoft.Xna.Framework;
+using MonoGame.Framework;
+
+namespace UnknownProject.Component.Miscellaneous.Tests
+{
+    [TestFixture()]
+    public class FPSCounterComponentTests
+    {
+        private GraphicConfiguration graphicConf;
+
+        private FPSCounterComponent presenter;
+        private FPSCounterComponent.IFPSCounterComponentView view;
+        private Vector2 textSize = new Vector2(20, 20); 
+
+        [SetUp]
+        public void Init()
+        {
+            view = A.Fake<FPSCounterComponent.IFPSCounterComponentView>();
+            A.CallTo(() => view.getSizeFromString(A<string>.Ignored)).Returns(textSize);
+
+            graphicConf = new GraphicConfiguration();
+            graphicConf.Width = 100;
+
+            presenter = new FPSCounterComponent(view, graphicConf);
+            presenter.Update(new GameTime()); // update first time as this is counted as a second, so the fps are now 1.
+        }
+
+        [Test()]
+        public void CalculateFramerateTest60FPS()
+        {
+            CalculateFrameRateTest(60);
+        }
+
+        [Test()]
+        public void CalculateFramerateTest150FPS()
+        {
+            CalculateFrameRateTest(150);
+        }
+
+        private void CalculateFrameRateTest(int fps)
+        {
+            // we need to actually round cause a long have no decimal places
+            long ticksPerFrame = Convert.ToInt64(Math.Round(TimeSpan.TicksPerSecond / (double)fps));
+
+            GameTime gameTime = new GameTime();
+            gameTime.ElapsedGameTime = TimeSpan.FromTicks(ticksPerFrame);
+
+            for (int i = 0; i < fps; i++)
+            {
+                presenter.Update(gameTime);
+            }
+
+            Assert.AreEqual(fps, presenter.CurrentFrameRate);
+        } 
+    }
+}
